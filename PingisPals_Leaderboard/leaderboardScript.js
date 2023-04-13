dd20e3bc56418b0094f3e69fbd887d09dca8aa17
LucasAsker <lucasasker@hotmail.com>
2023-04-13 15:10:43 +0200

    Leaderboard V1.3
    Jag skapade ett litet script som gör att spelarna skrivs ut med deras namn

--- PingisPals_Leaderboard/leaderboardScript.js
+++ PingisPals_Leaderboard/leaderboardScript.js
@@ -1,8 +1,41 @@
-let players = [
-    "player one",
-    "player two",
-    "player three",
-    "player four",
-    "player five"
-]
+let players = [{
+    placement: "1",
+    username: "player 1",
+    wins: "50",
+    winRatio: "3.0",
+    elo: "1200",
+    status: true
+},
+{
+    placement: "2",
+    username: "player 2",
+    wins: "50",
+    winRatio: "3.0",
+    elo: "1200",
+    status: true
+},
+{
+    placement: "3",
+    username: "player 3",
+    wins: "50",
+    winRatio: "3.0",
+    elo: "1200",
+    status: true
+},
+{
+    placement: "4",
+    username: "player 4",
+    wins: "50",
+    winRatio: "3.0",
+    elo: "1200",
+    status: true
+}]
+    
 
+for (let i = 0; i < players.length ; i++) {
+    const paragraph = document.createElement("h3");
+    const node = document.createTextNode(players.username[i]);
+    paragraph.appendChild(node);
+    const element = document.getElementById("ladder");
+    element.appendChild(paragraph);
+}
