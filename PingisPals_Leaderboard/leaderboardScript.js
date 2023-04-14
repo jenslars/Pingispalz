2decaf415406f3cbde1475420c040f5e0b2ab323
LucasAsker <lucasasker@hotmail.com>
2023-04-15 00:25:36 +0200

    Leaderboard v1.5 (in progres)
    Jag försökte jobba lite på top 3 funktionerna

--- PingisPals_Leaderboard/leaderboardScript.js
+++ PingisPals_Leaderboard/leaderboardScript.js
@@ -1,6 +1,6 @@
 let players = [{
     placement: "1",
-    username: "player 1",
+    username: "Lucas",
     wins: "50",
     winRatio: "3.0",
     elo: "1200",
@@ -8,7 +8,7 @@ let players = [{
 },
 {
     placement: "2",
-    username: "player 2",
+    username: "Oliver",
     wins: "50",
     winRatio: "3.0",
     elo: "1200",
@@ -16,7 +16,7 @@ let players = [{
 },
 {
     placement: "3",
-    username: "player 3",
+    username: "Henningsson",
     wins: "50",
     winRatio: "3.0",
     elo: "1200",
@@ -24,24 +24,69 @@ let players = [{
 },
 {
     placement: "4",
-    username: "player 4",
+    username: "Felix",
     wins: "50",
     winRatio: "3.0",
     elo: "1200",
     status: true
 }];
-    
-var table = "<tbody>"
-for (let i = 0; i < players.length ; i++) {
 
-        table += "<tr>";
-        table += "<td>" + players[i].placement + "</td>";
-        table += "<td>" + players[i].username + "</td>";
-        table += "<td>" + players[i].wins + "</td>";
-        table += "<td>" + players[i].winRatio + "</td>";
-        table += "<td>" + players[i].elo + "</td>";
-        table += "<td>" + players[i].status + "</td>";
-        table += "</tr>"
+//function ladder(players) {
+    var table = "<tbody>"
+    for (let i = 0; i < players.length ; i++) {
+
+            table += "<tr>";
+            table += "<td>" + players[i].placement + "</td>";
+            table += "<td>" + players[i].username + "</td>";
+            table += "<td>" + players[i].wins + "</td>";
+            table += "<td>" + players[i].winRatio + "</td>";
+            table += "<td>" + players[i].elo + "</td>";
+            table += "<td>" + players[i].status + "</td>";
+            table += "</tr>"
+    }
+    table += "</tbody>";
+    document.getElementById("ladder").innerHTML = table;
+    // } ladder();
+
+
+
+
+
+
+
+
+
+
+
+
+function topPlayers(players) {
+    let top3 = 3;
+    for (let i = 0; i < top3; i++)
+        if (players.placement == "1") {
+        let div = '<div id="top1">';
+        div;
+        div += "<h1>" + players.username + "</h1>";
+        div += "</div>";
+        document.getElementsById("topPlayers").innerHTML = div;
+    }
+        if (players.placement == "2")
+    {
+        let div = '<div id="top2">';
+        div;
+        div += "<h2>" + players.username + "</h2>";
+        div += "</div>";
+        document.getElementsById("topPlayers").innerHTML = div;
+    }
+        if (players.placement == "3")
+    {
+        let div = '<div id="top3">';
+        div;
+        div += "<h2>" + players.username + "</h2>";
+        div += "</div>";
+        document.getElementsById("topPlayers").innerHTML = div;
+    }
 }
-table += "</tbody>";
-document.getElementById("ladder").innerHTML = table;
+
+
+//topPlayers(players);
+
