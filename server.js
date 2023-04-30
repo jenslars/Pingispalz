2e7ded7e283b0a5254e4cacdd7621b0d9667a40c
Jens Larsen <jenslarsenslottis@gmail.com>
2023-04-30 07:43:54 +0200

    Updated backend server
    Inserted backend code from createOrg branch and new function that declares the logged in user with id globally

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ const port = 3000;
 app.use(express.static('images'));
 app.use(express.static('public'));
 
+//Declaring the user logged in globally 
+let loggedInUserId;
+
 app.get('/', (req, res) => {
   fs.readFile('index.html', function(error, data) {
     if (error) {
@@ -27,6 +30,19 @@ app.get('/', (req, res) => {
       res.write(data);
     }
     res.end();
+  });
+});
+
+app.get('/createorg', (req, res) => {
+  fs.readFile('createOrg.html', function(error, data) {
+      if (error) {
+          res.writeHead(404);
+          res.write('Error: File Not Found');
+      } else {
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.write(data);
+      }
+      res.end();
   });
 });
 
@@ -41,12 +57,13 @@ app.listen(3000, () => {
 
 app.use(express.json());
 
-// Handle POST requests to /register
 app.post('/register', async (req, res) => {
+  //function to register account
   const { email, username, password } = req.body;
 
   try {
     await pool.query('SELECT insert_user($1, $2, $3)', [email, username, password])
+    loggedInUserId = await pool.query('SELECT user_id from users WHERE email = $1', [email]);
     res.sendStatus(200); 
   } catch (err) {
     console.error(err);
@@ -59,10 +76,12 @@ app.post('/register', async (req, res) => {
 });
 
 app.post('/login', async (req, res) => {
+  //Function to validate users login input
   const { email, password } = req.body;
   
   try {
     const result = await pool.query('SELECT login($1, $2)', [email, password]);
+    loggedInUserId = await pool.query('SELECT user_id from users WHERE email = $1', [email]);
     const isValidLogin = result.rows[0].login;
     if (isValidLogin) {
       res.sendStatus(200);
@@ -73,6 +92,48 @@ app.post('/login', async (req, res) => {
     console.error(err);
     res.sendStatus(500);
   }
+});
+
+app.post('/createOrg', async (req, res) => {
+  //Function to insert data into table leaderboards and create new table for said leaderboard
+  const { tableName, tableDescription } = req.body;
+
+  const client = await pool.connect();
+  try {
+    await client.query('BEGIN');
+    
+    const leaderboardInsertResult = await client.query(`
+      INSERT INTO leaderboards (leaderboard_name, leaderboard_description, owner)
+      VALUES ($1, $2, $3)
+      RETURNING id
+    `, [tableName, tableDescription, user_id]);
+
+    const leaderboardId = leaderboardInsertResult.rows[0].id;
+
+    await client.query(`
+      CREATE TABLE ${tableName} (
+        server_id INTEGER REFERENCES leaderboards(id),
+        player_id INTEGER REFERENCES users(user_id),
+        elo INTEGER,
+        wins INTEGER,
+        losses INTEGER,
+        is_admin BOOLEAN
+      )
+    `);
+    await client.query(`
+      INSERT INTO ${tableName} (server_id, player_id, elo, wins, losses, is_admin)
+      VALUES ($1, $2, $3, $4, $5, $6)
+    `, [leaderboardId, loggedInUserId, 0, 0, 0, true]);
+
+    await client.query('COMMIT');
+  } catch (e) {
+    await client.query('ROLLBACK');
+    throw e;
+  } finally {
+    client.release();
+  }
+
+
 });
 
 app.get('/:page', (req, res) => {
@@ -88,5 +149,4 @@ app.get('/:page', (req, res) => {
     res.end();
   });
 });
-
 
